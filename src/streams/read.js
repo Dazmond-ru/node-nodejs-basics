35f448373f25d7029252e80e301fbb667533537b
Sergey <fserega2014@yandex.ru>
2023-06-20 00:02:26 +0300

    feat: add streams block solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs'
+import path from 'node:path'
+import url from 'node:url'
 
-await read();
+const read = async () => {
+  const __filename = url.fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt')
+
+  const stream = fs.createReadStream(filePath)
+
+  let content = ''
+  stream.on('data', (chunk) => (content += chunk))
+  stream.on('end', () => process.stdout.write(content))
+}
+
+await read()
